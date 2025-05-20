cd13b1c11b3f918354a51b73b1a60df02d50b9ab
232079 <it232079@trident.ac.jp>
2025-05-20 12:08:47 +0900

    動作テスト完了

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ GREEN = (0, 255, 0)
 DARK_GREEN = (0, 40, 0)
 
 # 画像ファイルのパス
-PLAYER_IMG_PATH = "/Users/github/sample/python/pygame/invader/sample03/assets/img/player.png"
-ALIEN_IMG_PATH = "/Users/github/sample/python/pygame/invader/sample03/assets/img/alien.png"
+PLAYER_IMG_PATH = "assets/img/player.png"
+ALIEN_IMG_PATH = "assets/img/alien.png"
 
 # 音声ファイルのパス
-SHOOT_SOUND_PATH = "/Users/github/sample/python/pygame/invader/sample03/assets/sound/shoot.mp3"
-HIT_SOUND_PATH = "/Users/github/sample/python/pygame/invader/sample03/assets/sound/hit.mp3"
-CLEAR_SOUND_PATH = "/Users/github/sample/python/pygame/invader/sample03/assets/sound/clear.mp3"
-GAMEOVER_SOUND_PATH = "/Users/github/sample/python/pygame/invader/sample03/assets/sound/gameover.mp3"
-GAMEPLAY_SOUND_PATH = "/Users/github/sample/python/pygame/invader/sample03/assets/sound/gameplay.mp3"
+SHOOT_SOUND_PATH = "assets/sound/shoot.mp3"
+HIT_SOUND_PATH = "assets/sound/hit.mp3"
+CLEAR_SOUND_PATH = "assets/sound/clear.mp3"
+GAMEOVER_SOUND_PATH = "assets/sound/gameover.mp3"
+GAMEPLAY_SOUND_PATH = "assets/sound/gameplay.mp3"
 
 # プレイヤークラス
 class Player(pygame.sprite.Sprite):
